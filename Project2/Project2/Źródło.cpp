cae792732434aca2868c3d31502a216798a6ed5d
lovir21 <oliregula@gmail.com>
2025-10-14 18:32:21 +0200

    2 zad

--- Project2/Project2/Źródło.cpp
+++ Project2/Project2/Źródło.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 int main()
 {
-	int x, y;
-	std::cin >> x >> y;
-	
-	if (x > y) {
+	int x, y, z;
+	std::cin >> x >> y >> z;
+	if (x > y and x > z) {
 		std::cout << x;
-	} 
-	else 
-	{
+	}
+	else if (y > x and y > z) {
 		std::cout << y;
 	}
+	else {
+		std::cout << z;
+	}
 }
+
+/*int x, y;
+	std::cin >> x >> y;
+
+	if (x > y) {
+		std::cout << x;
+	}
+	else
+	{
+		std::cout << y;
+	}*/
